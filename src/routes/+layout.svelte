abfb2be9624c9c4c8b02e1bf730c701ec5d13d93
kisi-dev <shirrogannie@gmail.com>
2025-09-29 11:35:54 +0700

    adding ld json

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,14 +8,14 @@
 <svelte:head>
 	<link rel="icon" href={favicon} />
 	<title>&gt;_kisilabs</title>
-	<meta name="description" content="kisilabs workspace" />
+	<meta name="description" content="Bagas/Kisi is an artist who design and code" />
 	<meta name="keywords" content="KiSI, kisi, kisilabs, Bagas Dwi, Personal Page, Portfolio, Resume, Projects, Workspace," />
 	<meta name="author" content="Bagas Dwi Anggoro" />
 	<meta name="robots" content="index,follow" />
 	<meta name="copyright" content="kisi" />
 
 	<meta property="og:title" content=">_kisilabs" />
-	<meta property="og:description" content="kisilabs workspace" />
+	<meta property="og:description" content="Bagas/Kisi is an artist who design and code" />
 	<meta property="og:url" content="https://kisilabs.space" />
 	<meta property="og:type" content="website" />
 	<meta property="og:image" content="/cover.avif" />
@@ -26,6 +26,37 @@
 	<meta name="twitter:card" content="summary" />
 	<meta name="twitter:site" content="@kisilabs" />
 	<meta name="twitter:creator" content="@kisilabs" />
+
+	<script type="application/ld+json">
+		{
+			"@context": "https://schema.org",
+			"@graph": [
+				{
+					"@type": "WebSite",
+					"name": ">_kisilabs",
+					"url": "https://kisilabs.space",
+					"potentialAction": {
+						"@type": "SearchAction",
+						"target": {
+							"@type": "EntryPoint",
+							"urlTemplate": "https://kisilabs.space/search?q={search_term_string}"
+						},
+						"query-input": "required name=search_term_string"
+					}
+				},
+				{
+					"@type": "SiteNavigationElement",
+					"name": "About",
+					"url": "https://kisilabs.space/about"
+				},
+				{
+					"@type": "SiteNavigationElement",
+					"name": "Contact",
+					"url": "https://kisilabs.space/contact"
+				}
+			]
+		}
+	</script>
 </svelte:head>
 
 {@render children?.()}
